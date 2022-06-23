9d365c554cd255bce28ac3a854cf9f8d990a6097
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-06-23 22:03:18 +1000

    Fixed listing simple form to support action text

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,10 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+  enum condition: {
+    needs_work: 0,
+    fair: 1,
+    brand_new: 2
+  }
 end
