1fef072cee2c367049c9d7e40764a902ef66729a
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-06-24 11:26:33 +1000

    install enum_help gem and modified new listing form for categories and conditions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
+  before_action :set_form_vars
 
   # GET /listings or /listings.json
   def index
@@ -62,6 +63,10 @@ class ListingsController < ApplicationController
     def set_listing
       @listing = Listing.find(params[:id])
     end
+
+    def set_form_vars
+      @categories = Category.all
+    end 
 
     # Only allow a list of trusted parameters through.
     def listing_params
