c3893a26d79942da37b9e94e021ec4cca3f506cc
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-06-24 15:01:27 +1000

    add validations for username & and set up associated validation for address

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,5 +2,7 @@ class Address < ApplicationRecord
   attribute :country, :string, default: "Australia"
   belongs_to :user
 
+  validates :postcode, length: { is:4 }
+
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
+
+# Runs validation for associated address and additional username field 
+  validates_associated :address
+  validates :address, presence: true 
+  validates_presence_of :username
+  validates_uniqueness_of :username  
+  validates_length_of :username, :minimum => 5, :maximum => 10  
+       
   
 end
