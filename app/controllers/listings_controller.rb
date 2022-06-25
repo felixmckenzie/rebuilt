578388b03bad6c71e4406010c6c6fb4605436101
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-06-25 15:44:17 +1000

    rails g model Order listing:references buyer:references seller:references

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,6 +2,7 @@ class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
   before_action :set_form_vars, only: [:new, :edit]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
 
   # GET /listings or /listings.json
   def index
@@ -69,8 +70,14 @@ class ListingsController < ApplicationController
       @categories = Category.all
     end 
 
+    def authorize_user
+      if listing.user_id != current_user.flash[:alert] = "You can't do that"
+        redirect_to listings_path
+      end
+    end
+
     # Only allow a list of trusted parameters through.
     def listing_params
-      params.require(:listing).permit(:title, :description, :condition, :price, :sold, :user_id, :category_id)
+      params.require(:listing).permit(:title, :description, :condition, :price, :sold, :user_id, :category_id, :picture)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :listings        
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
 
