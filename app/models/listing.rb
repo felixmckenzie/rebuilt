42b7aed3162c0ce1176d85ee0e1bae8ac2d30475
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-06-25 16:07:51 +1000

    validations for listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,10 +3,15 @@ class Listing < ApplicationRecord
   belongs_to :category
   has_rich_text :description
   has_one_attached :picture
+  has_one :order, dependent: :destroy 
+
+  # validations for listing form fields 
+  validates :title, :condition, :price, :description, presence: true
 
   enum condition: {
     needs_work: 0,
     fair: 1,
     brand_new: 2
   }
+  
 end
