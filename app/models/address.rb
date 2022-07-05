3fc97037f144b85e38ac13b87dcd3ffa3713dc59
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-07-05 12:03:29 +1000

    set state as enum on address.rb  and updated forms

--- app/models/address.rb
+++ app/models/address.rb
@@ -16,6 +16,17 @@ attribute :country, :string, default: "Australia"
 # validates :postcode, length: { is: 4 }
 # validates :postcode, format: { with: /\A[0-9]+\z/ } 
 
+enum state: {
+  NSW: 0, 
+  QLD: 1,
+  VIC: 3, 
+  TAS: 4,
+  SA: 5, 
+  WA: 6,
+  ACT: 7,
+  NT: 8, 
+}
+
 def address_to_string
   [ street_number, street_name, suburb, postcode, state ].compact.join(" ")
 end 
